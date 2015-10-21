a0a0b47a67c9c4972b020f6965f870eff7398526
Andrew Christensen <andrew.christensen@markit.com>
2015-10-21 10:20:40 -0600

    Answer comparison and Unit tests

--- BrowseOverflow/Answer.swift
+++ BrowseOverflow/Answer.swift
@@ -13,20 +13,72 @@ class Answer: NSObject, Comparable {
     var score = 0
     var person = Person(name: "", avatarLocation: "")
     var accepted = false
+    
+    func compare(answer: Answer) -> NSComparisonResult {
+        if self.accepted && !answer.accepted {
+            return .OrderedAscending
+        }
+        else if !self.accepted && answer.accepted {
+            return .OrderedDescending
+        }
+        if score > answer.score {
+            return .OrderedAscending
+        }
+        else if score < answer.score {
+            return .OrderedDescending
+        }
+        else {
+            return .OrderedSame
+        }
+    }
 }
 
 func <=(lhs: Answer, rhs: Answer) -> Bool {
-    return lhs.score <= rhs.score
+    if lhs.accepted && !rhs.accepted {
+        return false
+    }
+    else if !lhs.accepted && rhs.accepted {
+        return true
+    }
+    else {
+        return lhs.score <= rhs.score
+    }
 }
 
+// MARK: - Comparable
+
 func >(lhs: Answer, rhs: Answer) -> Bool {
-    return lhs.score > rhs.score
+    if lhs.accepted && !rhs.accepted {
+        return true
+    }
+    else if !lhs.accepted && rhs.accepted {
+        return false
+    }
+    else {
+        return lhs.score > rhs.score
+    }
 }
 
 func >=(lhs: Answer, rhs: Answer) -> Bool {
-    return lhs.score >= rhs.score
+    if lhs.accepted && !rhs.accepted {
+        return true
+    }
+    else if !lhs.accepted && rhs.accepted {
+        return false
+    }
+    else {
+        return lhs.score >= rhs.score
+    }
 }
 
 func <(lhs: Answer, rhs: Answer) -> Bool {
-    return lhs.score < rhs.score
+    if lhs.accepted && !rhs.accepted {
+        return false
+    }
+    else if !lhs.accepted && rhs.accepted {
+        return true
+    }
+    else {
+        return lhs.score < rhs.score
+    }
 }
--- BrowseOverflowTests/AnswerTests.swift
+++ BrowseOverflowTests/AnswerTests.swift
@@ -11,6 +11,7 @@ import XCTest
 class AnswerTests: XCTestCase {
     
     var answer = Answer()
+    var otherAnswer = Answer()
     
     // MARK: lifecycle
     
@@ -18,6 +19,9 @@ class AnswerTests: XCTestCase {
         answer.text = "The answer is 42."
         answer.person = Person(name: "Graham Lee", avatarLocation: "http://example.com/avatar.png")
         answer.score = 42
+        
+        otherAnswer.text = "I have the answer you need"
+        otherAnswer.score = 42
     }
     
     // MARK: properties
@@ -41,5 +45,24 @@ class AnswerTests: XCTestCase {
     
     func testAnswerHasAScore() {
         XCTAssertEqual(answer.score, 42)
+    }
+    
+    func testAcceptedAnswerCombesBeforeUnaccepted() {
+        otherAnswer.accepted = true
+        otherAnswer.score = answer.score + 10
+        
+        XCTAssertEqual(answer.compare(otherAnswer), NSComparisonResult.OrderedDescending)
+        XCTAssertEqual(otherAnswer.compare(answer), NSComparisonResult.OrderedAscending)
+    }
+    
+    func testAnswersWithEqualScoresCompareEqually() {
+        XCTAssertEqual(answer.compare(otherAnswer), NSComparisonResult.OrderedSame)
+        XCTAssertEqual(otherAnswer.compare(answer), NSComparisonResult.OrderedSame)
+    }
+    
+    func testLowerScoringAnswerComesAfterHigher() {
+        otherAnswer.score = answer.score + 10
+        XCTAssertEqual(answer.compare(otherAnswer), NSComparisonResult.OrderedDescending)
+        XCTAssertEqual(otherAnswer.compare(answer), NSComparisonResult.OrderedAscending)
     }
 }
